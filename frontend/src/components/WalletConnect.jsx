1e571d890ca5e60b2211efea2306c4d62926097f
Mars Ifeanyi <marschalice@gmail.com>
2024-04-02 20:09:30 +0100

    Create WalletConnect component

---
+++ frontend/src/components/WalletConnect.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useConnectWallet } from "@web3-onboard/react";
+
+const WalletConnect = () => {
+    const [{ wallet }, connect, disconnect] = useConnectWallet();
+    return (
+        <div>
+            {!wallet?.accounts ? (
+                <button
+                    onClick={() => connect()}
+                    type="button"
+                    className="font-medium rounded-lg text-sm px-4 py-3 text-center bg-white text-black"
+                >
+                    Connect Wallet
+                </button>
+            ) : (
+                <button
+                    onClick={() => disconnect()}
+                    type="button"
+                    className="font-medium rounded-lg text-sm px-4 py-3 text-center bg-white text-black"
+                >
+                    Disconnect Wallet
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default WalletConnect;
