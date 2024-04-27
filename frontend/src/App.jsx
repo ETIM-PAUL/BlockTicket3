bb1be9e59dc5331b2dd2d88743080ffd7fc4ebd9
UgoMars <0xmarsifeanyi@gmail.com>
2024-04-27 13:13:07 +0100

    Update App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,14 @@ import TopNav from "./layout/TopNav";
 
 const App = () => {
     return (
-        <div className="  h-screen">
-            <TopNav />
+        <div className="   h-screen">
+            {/* <TopNav /> */}
 
             <GraphQLProvider></GraphQLProvider>
 
             <div>
                 {/* <MainNav /> */}
+
                 <HomePage />
             </div>
         </div>
