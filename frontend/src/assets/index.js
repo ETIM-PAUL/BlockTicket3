0b48ea5e0540bbb6a5c29d2a1c6766d3e3ea79dc
UgoMars <0xmarsifeanyi@gmail.com>
2024-04-27 13:09:00 +0100

    Add Images

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -9,9 +9,23 @@ import desertKing03 from "./desertKing17.png";
 import eventBgImage from "./eventSet2.jpg";
 import eventBgImage2 from "./eventSet.jpg";
 import eventBgImage3 from "./EventSet3.jpg";
+import heroImage from "./HeroImage.png";
+import bgImage from "./BgImage.png";
+import event1 from "./event1.png";
+import event2 from "./event2.png";
+import event3 from "./event3.png";
+import event4 from "./event4.png";
+import event5 from "./event5.png";
+import event6 from "./event6.png";
+import event7 from "./event7.png";
+import blog1 from "./blog1.png";
+import blog2 from "./blog2.png";
+import blog3 from "./blog3.png";
+import cartesi from "./cartesi.png";
 
 export {
     blockTicket3Logo,
+    cartesi,
     facebook,
     linkedin,
     twitter,
@@ -22,4 +36,16 @@ export {
     eventBgImage,
     eventBgImage2,
     eventBgImage3,
+    heroImage,
+    bgImage,
+    event1,
+    event2,
+    event3,
+    event4,
+    event5,
+    event6,
+    event7,
+    blog1,
+    blog2,
+    blog3,
 };
