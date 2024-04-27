6b462b98206852b5867637a7aeda2bcf4fdddef2
UgoMars <0xmarsifeanyi@gmail.com>
2024-04-27 13:22:18 +0100

    Update file

--- frontend/src/components/modals/VoteModal.tsx
+++ frontend/src/components/modals/VoteModal.tsx
@@ -21,25 +21,29 @@ const VoteModal = ({ isVisible, onClose }: Props) => {
                 id="wrapper"
                 onClick={handleClose}
             >
-                <div className="w-[500px]  bg-black backdrop-blur-none z-50 flex flex-col relative ">
-                    <div className=" p-2 rounded">
+                <div className="w-[550px]    bg-gradient-to-l from-[#5522CC] to-[#ED4690] backdrop-blur-none z-50 flex flex-col relative ">
+                    <div className=" p-10 rounded">
                         <button
-                            className="text-white text-2xl absolute top-6 right-10   "
+                            className="text-white text-2xl absolute top-4 right-8   "
                             onClick={() => onClose()}
                         >
                             X
                         </button>
 
-                        <div className="bg-black   z-50  mx-auto flex flex-col mt-2  p-4  ">
-                            <h2 className="text-white font-bold text-2xl text-center font-nexa">
+                        <div className="  bg-gradient-to-r from-[#5522CC] to-[#ED4690]   z-50  mx-auto flex flex-col mt-2  p-4  ">
+                            <h2 className="text-white font-bold text-xl text-center font-nexa pt-4">
                                 Give Your Vote
                             </h2>
+                            <p className="text-center mt-4">
+                                You Vote counts we want to hear from you on the
+                                quality of the event.
+                            </p>
 
                             <div className="flex flex-row mt-10 justify-between gap-4 py-6 ">
                                 {/* Up Vote */}
                                 <div className="flex w-full">
                                     <button
-                                        className="text-lg font-semibold justify-center p-4 text-black bg-white w-full flex hover:bg-[#292929] hover:text-white"
+                                        className="text-lg font-semibold justify-center p-4 text-black  w-full flex bg-white  hover:bg-gradient-to-r hover:from-[#9a8abd] hover:to-[#5946ed] hover:text-[#FFFFFF]"
                                         onClick={() => setShowModal(true)}
                                     >
                                         Up Vote
@@ -48,7 +52,7 @@ const VoteModal = ({ isVisible, onClose }: Props) => {
 
                                 {/* Down Vote*/}
                                 <div className="flex w-full">
-                                    <button className="text-lg font-semibold text-center justify-center p-4 bg-[#292929] text-white w-full  flex hover:bg-white hover:text-black ">
+                                    <button className="text-lg font-semibold text-center justify-center p-4 bg-gradient-to-r from-[#5522CC] to-[#8352f5]  text-white hover:bg-gradient-to-r hover:from-[#9a8abd] hover:to-[#5946ed] hover:text-[#FFFFFF]">
                                         Down Vote
                                     </button>
                                 </div>
