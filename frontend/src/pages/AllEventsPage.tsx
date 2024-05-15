4f10336d966263b14d1c2112b1edb1eda8aef7ec
UgoMars <0xmarsifeanyi@gmail.com>
2024-05-15 18:38:57 +0100

    Fix Footer Responsiveness

--- frontend/src/pages/AllEventsPage.tsx
+++ frontend/src/pages/AllEventsPage.tsx
@@ -10,28 +10,35 @@ const AllEventsPage = (props: Props) => {
     const [connectedWallet] = useWallets();
 
     return (
-        <div className="bg-white flex flex-col h-full w-full">
+        <div className=" bg-gradient-to-l from-[#5522CC] to-[#ED4690] flex flex-col min-h-screen">
+            {" "}
+            {/* Updated class */}
             <TopNav />
-            <div className="md:h-[89vh]">
-                {connectedWallet ?
+            <main className="flex-grow">
+                {" "}
+                {/* New main tag with flex-grow */}
+                {connectedWallet ? (
                     <AllEvents />
-                    :
-                    <div className="h-full flex flex-col">
+                ) : (
+                    <div className="h-full flex flex-col ">
                         <div className="w-full bg-[#EEE1FF] h-2"></div>
 
                         <div className="bg-base-100 h-full py-8 px-10 md:px-24 bg-gradient-to-l from-[#5522CC] to-[#ED4690]">
-
-                            <div
-                                className="text-white font-medium text-lg md:text-3xl mt-10 "
-                            >
-                                <h3>Please Connect Your Wallet to Fetch All Events</h3>
+                            <div className="text-white font-medium text-lg md:text-3xl mt-10 ">
+                                <h3>
+                                    Please Connect Your Wallet to Fetch All
+                                    Events
+                                </h3>
                             </div>
                         </div>
-                        <div className="w-full bg-[#EEE1FF] h-2"></div>
-                        <Footer />
                     </div>
-                }
-            </div>
+                )}
+            </main>
+            <footer className="mt-auto">
+                <div className="w-full bg-[#EEE1FF] h-2"></div>
+
+                <Footer />
+            </footer>
         </div>
     );
 };
